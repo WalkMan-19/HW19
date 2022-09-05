18b4fa5d8849d9f8a70b3460228a4596b2fa2edb
Nadir <yusipov.nadir@gmail.com>
2022-09-05 18:45:46 +0300

    добавлены пользователи

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restx import Api
-
+from dao.model.user import User
 from config import Config
 from setup_db import db
 from views.directors import director_ns
@@ -27,8 +27,21 @@ def register_extensions(app):
     api.add_namespace(auth_ns)
 
 
+def create_data(db, app):
+    with app.app_context():
+        db.create_all()
+
+        u1 = User(username="vasya", password="my_little_pony", role="user")
+        u2 = User(username="oleg", password="qwerty", role="user")
+        u3 = User(username="oleg", password="P@ssw0rd", role="admin")
+
+        with db.session.begin():
+            db.session.add_all([u1, u2, u3])
+
+
 app = create_app(Config())
 app.debug = True
 
 if __name__ == '__main__':
+    create_data(db, app)
     app.run(host="localhost", port=10001, debug=True)
